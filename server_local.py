e1b98a037a2cedaad1c602ab2ec4b617b6131873
Johann Yamil <johannyamiljimenezperez@gmail.com>
2026-02-17 23:07:17 -0600

    puerto

--- server_local.py
+++ server_local.py
@@ -1,8 +1,9 @@
+import os
 from wsgiref.simple_server import make_server
 from app import application  # Importa la función 'application' de tu app.py
 
 if __name__ == "__main__":
-    port = 8000
+    port = int(os.environ.get("PORT", 8000))
     print(f" Servidor de desarrollo activo en: http://localhost:{port}")
     
     # Creamos el servidor local
